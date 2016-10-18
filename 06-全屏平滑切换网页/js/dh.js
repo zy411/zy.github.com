dad7522eca25e535d3f1e718ff246d91431d1271
zhangyi <776585686@qq.com>
2016-10-18 14:51:50 +0800

    *

---
+++ 06-全屏平滑切换网页/js/dh.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".content").fullpage({
+        //sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#4BBFC3', '#1bbc9b'],
+        'navigation': true,
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        menu:'#menu',
+        verticalCentered:false
+
+    })
+})
